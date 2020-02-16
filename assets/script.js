23545e6c8de26d81d9a40adbdeefac377efa7ccd
Shawn Wright <volushawn@gmail.com>
2020-02-16 12:36:25 -0600

    pseudo planning and basic HTML layout

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+/* 
+Will rely on momentJS for easy date formatting, jQuery for DOM manipulation and ajax API requests.
+
+will need access to DOM elements:
+search field, submit button (click event listener), previous search city list (event delegation for these dynamic buttons), search results content area, 5-day forecast content area
+
+search results: current and forecast areas will be fully cleared and created dynamically whenever a new set of results are returned
+*/
+
+function 
+
+
+
