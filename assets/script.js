055084f2eaaf685c6a3ced456e51b9560d7b61f6
Shawn Wright <volushawn@gmail.com>
2020-02-18 02:03:55 -0600

    Current weather icon is displaying. Building out forecast function to parse results and generate forecast cards

--- assets/script.js
+++ assets/script.js
@@ -11,6 +11,7 @@ const $cityInput = document.querySelector("#cityInput");
 const $searchHistory = document.querySelector("#recentSearches");
 
 let apiKey = "87afb66ebd77a3851e1a006191d29852";
+let iconURL = "http://openweathermap.org/img/w/";
 let searchHistory = {
     cities: 
     []
@@ -31,7 +32,7 @@ $("document").ready(function() {
     //listen for search history clicks
     $("document").on("click", ".previousSearch", function(event){
        let cityName = $(this).text();
-       console.log(cityName);
+       getWeatherResults(cityName);
     });
 
     //listen for new city search
@@ -63,6 +64,7 @@ $("document").ready(function() {
             method: "GET"
           }).then(function(response) {
             console.log(response);
+            updateForecastDisplay(response.list);
           });
 
 
@@ -72,22 +74,30 @@ $("document").ready(function() {
     //parse current weather response data and paint to the DOM
     //this function will be responsible for gathering UV index information, as the response data has the requisite lat/lon data needed to get UVI
     function updateCurrentWeatherDisplay(response){
+        $("#currentWeatherBox").removeClass('hidden');
+
         let currentWeather = response;
-        console.log(currentWeather);
         let date = getDate(parseInt(currentWeather.dt));
         let city = currentWeather.name;
         let temp = currentWeather.main.temp;
         let humidity = currentWeather.main.humidity;
         let wind = currentWeather.wind.speed;
-        let uvIndex = getUVIndex(currentWeather.coord);
+        getUVIndex(currentWeather.coord);
+        let iconSRC = iconURL + (currentWeather.weather[0].icon) + ".png";
 
         $("#currentWeatherCityName").text(city + " " + date);
+        $("#weatherIcon").attr("src", iconSRC).attr("alt", currentWeather.weather[0].main);
         $("#currentTemperature").text(temp);
         $("#currentHumidity").text(humidity);
         $("#currentWindSpeed").text(wind);
+    }
 
-
-
+    //function which accepts forecast object data and populates 5 days worth of cards with the forecase
+    function updateForecastDisplay(forecast){
+        for (i in forecast){
+            console.log(getDate(forecast[i].dt));
+            
+        }
     }
 
     //function which accepts a unix millisecond integer and returns a formatted date string
@@ -95,7 +105,7 @@ $("document").ready(function() {
         return(moment.unix(time).format('l'));
     }
 
-    //function takes a coordinates object and returns a UV index integer
+    //function takes a coordinates object sets the DOM element directly when results are returned
     function getUVIndex(coordinates){
         let uvqueryURL = "http://api.openweathermap.org/data/2.5/uvi?lon=" + coordinates.lon + "&lat=" + coordinates.lat + "&appid=" + apiKey;
         $.ajax({
@@ -103,13 +113,8 @@ $("document").ready(function() {
             method: "GET"
           }).then(function(response) {
             $("#currentUV").text(response.value);
-
-            //console.log(response.value);
-            //return (response.value);
         }); 
-
     }
-
 });
 
 
