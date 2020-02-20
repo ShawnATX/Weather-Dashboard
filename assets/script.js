865a6ed3f2e3df3efe2bb6749beb79f622b55490
Shawn Wright <volushawn@gmail.com>
2020-02-19 20:11:10 -0600

    Weather forecast is loading dynamically as expected

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,6 @@
-/* 
-Will rely on momentJS for easy date formatting, jQuery for DOM manipulation and ajax API requests.
-
-will need access to DOM elements:
-search field, submit button (click event listener), previous search city list (event delegation for these dynamic buttons), search results content area, 5-day forecast content area
-
-search results: current and forecast areas will be fully cleared and created dynamically whenever a new set of results are returned
-*/
-
-const $cityInput = document.querySelector("#cityInput");
+const $cityInput = $("#cityInput");
 const $searchHistory = $("#recentSearches");
+const $forecastArea = $("#forecastArea");
 
 let apiKey = "87afb66ebd77a3851e1a006191d29852";
 let iconURL = "http://openweathermap.org/img/w/";
@@ -43,28 +35,28 @@ function updateSearchHistoryList(){
     for (city in searchHistory.cities) {
         console.log(searchHistory.cities[city]);
         let newSearchItem = $("<li>");
-        newSearchItem.attr("class", "list-group-item previousSearch").text(searchHistory.cities[city]);
+        newSearchItem.attr("class", "list-group-item previousSearch m-1").text(searchHistory.cities[city]);
         $searchHistory.append(newSearchItem);
     }
-
 }
  
 
 $("document").ready(function() {
     //listen for search history clicks. May want to refactor to pass cityID for repeated searches
-    $("document").on("click", ".previousSearch", function(event){
+    $("document").on("click", ".previousSearch", function(){
+        console.log(this);
        let cityName = $(this).text();
        getWeatherResults(cityName);
     });
 
     //listen for new city search
     $("#searchSubmit").on("click", function(event){
-        let cityName = $("#cityInput").val();
+        let cityName = $cityInput.val();
         cityName =  cityName.trim();
         getWeatherResults(cityName);
     });
 
-    //perform API requests for a specific city
+    //perform API requests for a specific city for both current and forecasted data
     function getWeatherResults(city){
         let currentQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + apiKey;
         let forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + apiKey;
@@ -124,13 +116,56 @@ $("document").ready(function() {
         $("#currentWeatherBox").append(uvP);
     }
 
-    //function which accepts forecast object data and populates 5 days worth of cards with the forecase
-    function updateForecastDisplay(forecast){
-        for (i in forecast){
-            console.log(getDate(forecast[i].dt));
 
+    //function which accepts forecast object data and populates 5 days worth of cards with the forecast
+    function updateForecastDisplay(forecast){
+        //using forecast indexes 2, 10, 18, 26, 34 - this represents one forecast for each day. A more thorough approach would be to digest all data for each day and output relevant averages/min/max values
+        let forecastArray = [forecast[2], forecast[10], forecast[18], forecast[26], forecast[34]];
+        for (i in forecastArray){
+            let nextCard = $("<div>");
+            nextCard.attr("class", "card mx-3").attr("style", "width: 10rem;");
+            let cardBody = $("<div>");
+            cardBody.attr("class", "card-body");
+            let cardHeading = $("<h5>");
+            cardHeading.attr("class", "card-title");
+            cardHeading.text(getDate(forecastArray[i].dt));
+            let icon = $("<img>");
+            icon.attr("src", iconURL + (forecastArray[i].weather[0].icon) + ".png");
+            let tempP = $("<p>");
+            tempP.attr("class", "card-text");
+            tempP.text("Temp: " + forecastArray[i].main.temp + "°F");
+            let humidP = $("<p>");
+            humidP.attr("class", "card-text");
+            humidP.text("Humidity: " + forecastArray[i].main.humidity);
+
+            cardBody.append(cardHeading);
+            cardBody.append(icon);
+            cardBody.append(tempP);
+            cardBody.append(humidP);
+            nextCard.append(cardBody);
+            $forecastArea.append(nextCard);
         }
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //function which accepts a unix millisecond integer and returns a formatted date string
     function getDate(time){
@@ -153,5 +188,6 @@ $("document").ready(function() {
 
 //invoking localStorage check (hopefully) while document is loading
 getStoredData();
+updateSearchHistoryList();
 
 
